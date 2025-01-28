29217231d507a6af33dad5ac35cccdb9329c9aea
Sofiia Kulish <sofiia.kulish@webpros.com>
2025-01-28 11:29:42 +0100

    PHP 8.4 support. fgetcsv(): the $escape parameter must be provided as its default value will change

--- library/Zend/Translate/Adapter/Csv.php
+++ library/Zend/Translate/Adapter/Csv.php
@@ -89,7 +89,7 @@ class Zend_Translate_Adapter_Csv extends Zend_Translate_Adapter
             throw new Zend_Translate_Exception('Error opening translation file \'' . $filename . '\'.');
         }
 
-        while(($data = fgetcsv($this->_file, $options['length'], $options['delimiter'], $options['enclosure'])) !== false) {
+        while(($data = fgetcsv($this->_file, $options['length'], $options['delimiter'], $options['enclosure'], '\\')) !== false) {
             if (substr($data[0], 0, 1) === '#') {
                 continue;
             }
